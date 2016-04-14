9207e4014bd3f7d271e817c9f7e36ac943396f93
Cole MacLean <maclean.cole@gmail.com>
2016-04-14 16:15:28 +0200

    embedded netowrk graphic into beaker file. Node and edge static histograms developed.

--- Final Project/sc_network.js
+++ Final Project/sc_network.js
@@ -1,9 +1,7 @@
-function draw_network(network_data,world,div_name){
 
-  var network_data = beaker.network_data
+function draw(world_data,network_data,div_name){
 
-  var world = beaker.world_data
-
+    //page update function called after any changes
   var width = 1300,
       height = 600;
 
@@ -26,34 +24,66 @@ function draw_network(network_data,world,div_name){
       .attr("class", "graticule")
       .attr("d", path);
 
-  svg.insert("path", ".graticule")
-      .datum(topojson.feature(world, world.objects.land))
-      .attr("class", "land")
-      .attr("d", path);
+  d3.select("#nNode")
+  .attr("max",network_data["nodes"].length)
+  .attr("value",network_data["nodes"].length)
+  .on("input", function() {
+    update(+this.value);
+  });
 
-  svg.insert("path", ".graticule")
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
-      .attr("class", "boundary")
-      .attr("d", path);
+    function update(input_node){
+      input_node = +input_node // convert string input to int for equality evaluations
+
+      d3.selectAll(".connection").remove();
 
       var connection = svg.selectAll(".connection")
-    .data(network_data['links'])
-    .enter().append("g")
-    .attr("class","connection")
-    .append("line")
-    .style("stroke","black")
-    .attr("x1", function(d){return projection(d['lon_lat_1'])[0]})
-    .attr("y1", function(d){return projection(d['lon_lat_1'])[1]})
-    .attr("x2", function(d){return projection(d['lon_lat_2'])[0]})
-    .attr("y2", function(d){return projection(d['lon_lat_2'])[1]})
+      .data(network_data['links'])
+      .enter().append("g")
+      .attr("class","connection")
+      .filter(function(d) { return +d.second_node <= input_node; })
+      .append("line")
+      .attr("class","link")
+      .style("stroke","black")
+      .attr("x1", function(d){return projection(d['lon_lat_1'])[0]})
+      .attr("y1", function(d){return projection(d['lon_lat_1'])[1]})
+      .attr("x2", function(d){return projection(d['lon_lat_2'])[0]})
+      .attr("y2", function(d){return projection(d['lon_lat_2'])[1]})
 
-    var node = svg.selectAll(".node")
-    .data(network_data['nodes'])
-    .enter().append("g")
-    .attr("class","node")
-    .append("circle")
-    .attr("cx", function (d) {return projection(d['GPS_lon_lat'])[0]; })
-    .attr("cy", function (d) {return projection(d['GPS_lon_lat'])[1]; })
-    .attr("r", "2px")
-    .attr("fill", "red")
- };
+      // connection.exit().remove();
+
+      d3.selectAll(".node").remove();
+
+      var node = svg.selectAll(".node")
+      .data(network_data['nodes'], function(d){ return d.SC_index})
+      .enter().append("g")
+      .attr("class","node")
+      .filter(function(d) { return +d.SC_index <= input_node; })
+      .append("circle")
+      .attr("class","SC")
+      .attr("cx", function (d) {return projection(d['GPS_lon_lat'])[0]; })
+      .attr("cy", function (d) {return projection(d['GPS_lon_lat'])[1]; })
+      .attr("r", "2px")
+      .attr("fill", "red")
+
+      d3.select("#nNode-value").text(input_node);
+    }
+
+  //build discipline selection pane
+  function draw_world(world) {
+    debugger;
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.feature(world, world.objects.land))
+        .attr("class", "land")
+        .attr("d", path);
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+        .attr("class", "boundary")
+        .attr("d", path);
+    }
+    debugger;
+    draw_world(world_data);
+    //initial draw call on page load
+    update(network_data["nodes"].length);
+  };
